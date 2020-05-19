f3bdf3f2436ee10e4c8114947c57fc268132c51b
Hkwolfx <bourlandalexis@orange.fr>
2020-05-19 13:46:43 +0200

    index changes

--- pages/index.js
+++ pages/index.js
@@ -92,10 +92,87 @@ const Index = () => {
                                 <div className="back text-center">
                                     <Link href="/categories/photographie">
                                         <a>
-                                            <h3 className="h1">photographie</h3>
+                                            <h3 className="h1">Photographie</h3>
                                         </a>
                                     </Link>
                                     <p className="lead">Partagez vos meilleurs shots !</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="flip flip-horizontal">
+                                <div
+                                    className="front"
+                                    style={{
+                                        backgroundImage:
+                                            'url(' +
+                                            'https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg' +
+                                            ')'
+                                    }}
+                                >
+                                    <h2 className="text-shadow text-center h1">Art-Vivant</h2>
+                                </div>
+                                <div className="back text-center">
+                                    <Link href="/categories/art-vivant">
+                                        <a>
+                                            <h3 className="h1">Art-Vivant</h3>
+                                        </a>
+                                    </Link>
+                                    <p className="lead">Oui c'est de l'art !!!</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="flip flip-horizontal">
+                                <div
+                                    className="front"
+                                    style={{
+                                        backgroundImage:
+                                            'url(' +
+                                            'https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg' +
+                                            ')'
+                                    }}
+                                >
+                                    <h2 className="text-shadow text-center h1">Art-Moderne</h2>
+                                </div>
+                                <div className="back text-center">
+                                    <Link href="/categories/art-moderne">
+                                        <a>
+                                            <h3 className="h1">Art Moderne</h3>
+                                        </a>
+                                    </Link>
+                                    <p className="lead">
+                                        Partagez nous vos démos, on adore !
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="flip flip-horizontal">
+                                <div
+                                    className="front"
+                                    style={{
+                                        backgroundImage:
+                                            'url(' +
+                                            'https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg' +
+                                            ')'
+                                    }}
+                                >
+                                    <h2 className="text-shadow text-center h1">Littérature</h2>
+                                </div>
+                                <div className="back text-center">
+                                    <Link href="/categories/litterature">
+                                        <a>
+                                            <h3 className="h1">Littérature</h3>
+                                        </a>
+                                    </Link>
+                                    <p className="lead">Parlez-nous de vos oeuvres cultes ...</p>
                                 </div>
                             </div>
                         </div>
